609b40095c85220e8a8531a577046e74d6a6634f
radmanplays <radmanfprn@gmail.com>
2023-11-08 09:44:23 +0330

    uploading

---
+++ README.md
@@ -0,0 +1,2 @@
+# ayuncraft source
+## This repository will be deleted imminently. Create a fresh fork ASAP to preserve it, you MUST fork a 100% fresh copy in order for the repository to be considered genuine
---
+++ src/main/java/net/lax1dude/eaglercraft/ConfigConstants.java
@@ -0,0 +1,26 @@
+package net.lax1dude.eaglercraft;
+
+import java.util.regex.Pattern;
+
+public class ConfigConstants {
+	
+	public static boolean profanity = false;
+
+	public static final String version = "22w15d";
+	public static final String mainMenuString = "ayuncraft " + version;
+	
+	public static final String forkMe = "https://github.com/ayunami2000/ayuncraft";
+
+	public static final boolean html5build = true;
+
+	public static String[] proxies = new String[]{"pproxy.rom1504.fr","webmcproxy.glitch.me","net-browserify.glitch.me"};
+
+	public static Pattern ipPattern = Pattern.compile("^"
+			+ "(((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}" // Domain name
+			+ "|"
+			+ "localhost" // localhost
+			+ "|"
+			+ "(([0-9]{1,3}\\.){3})[0-9]{1,3})" // Ip
+			+ "(:"
+			+ "[0-9]{1,5})?$"); // Port
+}
